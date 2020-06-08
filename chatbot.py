7d01e99b9741c7e7a1ce434392487052de60bfe7
Diegodevops26 <63413446+Diegodevops26@users.noreply.github.com>
2020-06-08 20:58:24 -0300

    Add files via upload
    Código em python

---
+++ chatbot.py
@@ -0,0 +1,18 @@
+from chatterbot.trainers import ListTrainer
+from chatterbot import ChatBot
+
+bot = ChatBot('TW Chat Bot')
+
+conversa = ['Oi', 'Olá', 'Tudo bem?', 'Tudo ótimo', 
+            'Você gosta de programar?', 'Sim, eu programo em Python']
+
+bot.set_trainer(ListTrainer)
+bot.train(conversa)
+
+while True:
+    pergunta = input("Usuário: ")
+    resposta = bot.get_response(pergunta)
+    if float(resposta.confidence) > 0.5:
+        print('TW Bot: ', resposta)
+    else:
+        print('TW Bot: Ainda não sei responder esta pergunta')
